e25dae41ec8d2e4f428511cb469b550e8b5cf0b3
jongjerry123 <jongjerry123@gmail.com>
2025-04-23 11:03:40 +0900

    다시푸시

---
+++ src/main/java/com/project/irumi/board/model/dto/PostReportDTO.java
@@ -0,0 +1,47 @@
+package com.project.irumi.board.model.dto;
+
+import java.time.LocalDateTime;
+
+public class PostReportDTO {
+    private Long reportId;
+    private Long targetId;
+    private String reportReason;
+    private String reportedBy;
+    private LocalDateTime reportDate;
+
+    public PostReportDTO() {}
+
+    public PostReportDTO(Long reportId, Long targetId, String reportReason, String reportedBy, LocalDateTime reportDate) {
+        this.reportId = reportId;
+        this.targetId = targetId;
+        this.reportReason = reportReason;
+        this.reportedBy = reportedBy;
+        this.reportDate = reportDate;
+    }
+
+    public Long getReportId() { return reportId; }
+    public void setReportId(Long reportId) { this.reportId = reportId; }
+
+    public Long getTargetId() { return targetId; }
+    public void setTargetId(Long targetId) { this.targetId = targetId; }
+
+    public String getReportReason() { return reportReason; }
+    public void setReportReason(String reportReason) { this.reportReason = reportReason; }
+
+    public String getReportedBy() { return reportedBy; }
+    public void setReportedBy(String reportedBy) { this.reportedBy = reportedBy; }
+
+    public LocalDateTime getReportDate() { return reportDate; }
+    public void setReportDate(LocalDateTime reportDate) { this.reportDate = reportDate; }
+
+    @Override
+    public String toString() {
+        return "PostReportDTO{" +
+                "reportId=" + reportId +
+                ", targetId=" + targetId +
+                ", reportReason='" + reportReason + '\'' +
+                ", reportedBy='" + reportedBy + '\'' +
+                ", reportDate=" + reportDate +
+                '}';
+    }
+}
