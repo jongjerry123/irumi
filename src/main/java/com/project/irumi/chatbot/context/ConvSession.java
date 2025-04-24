3e4d6d7405af45dcb58bca3b2c7ea5af5587b3e5
seonbint <seonbint@gmail.com>
2025-04-24 19:57:26 +0900

    convsession 필드에 subtopicid 추가

--- src/main/java/com/project/irumi/chatbot/context/ConvSession.java
+++ src/main/java/com/project/irumi/chatbot/context/ConvSession.java
@@ -6,11 +6,13 @@ import java.util.UUID;
 
 public class ConvSession {
 	private final String convId = UUID.randomUUID().toString();
-    private final String topic;
+	private final String topic;          // 대화 주제: job / spec / act / schedule
+    private String subtopicId = null;    // 서브 주제 ID (직무 ID or 스펙 ID 등)
     private String userId;
     private final List<String> contextHistory = new ArrayList<>();
 
     public ConvSession(String topic, String userId) {
+    	// 현재는 subtopic이 세션 중에 변경 가능한 주제인듯?
         this.topic = topic;
         this.userId = userId;
     }
@@ -37,6 +39,14 @@ public class ConvSession {
 
 	public void setUserId(String userId) {
 		this.userId = userId;
+	}
+
+	public String getSubtopicId() {
+		return subtopicId;
+	}
+
+	public void setSubtopicId(String subtopicId) {
+		this.subtopicId = subtopicId;
 	}
 
 }
